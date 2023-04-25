2d1df7b89890059e26eb3e215540a9d9737d95fe
coxan33 <coe250@hotmail.com>
2023-04-25 07:08:56 -0700

    add strcpy function

--- aux_functions.c
+++ aux_functions.c
@@ -7,4 +7,12 @@ int _strlen(char *s)
         continue;
     return (i);
 }
+char *_strcpy(char *dest, char *src)
+{
+	char *copy = dest;
+
+	while ((*copy++ = *src++) != '\0')
+		continue;
+	return (dest);
+}
 
