11aa663185e0f4911381ac29be152bc12bac8df9
MateoOlv <6304@holbertonschool.com>
2023-04-25 14:18:45 +0000

    execute command function added

---
+++ executecommand.c
@@ -0,0 +1,20 @@
+int execommand(char *string, char **array, int *pcount)
+{
+    int returncommand;
+    
+    array[0] = string;
+    returncommand = executePathProgram(array);
+    
+    if (_strcmp(string, "/usr/bin/env"))
+        free(string);
+
+    (*pcount)++;
+    
+    if (returncommand == -1)
+    {
+        fprintf(stderr, "./hsh: %d: %s: not found\n", *pcount, array[0]);
+        returncommand = 127;
+    }
+    
+    return returncommand;
+}
