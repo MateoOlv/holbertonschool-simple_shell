97246fbd5a51d5f3e65d22fe36a21ed52cc484d3
MateoOlv <6304@holbertonschool.com>
2023-04-25 14:20:43 +0000

    get env clone function

---
+++ _getenv.c
@@ -0,0 +1,17 @@
+char *_getenv(char *len)
+{
+    char *env = NULL;
+    char *val = NULL;
+    int leng = _strlen(len);
+
+    for (int i = 0; environ[i]; i++)
+    {
+        env = environ[i];
+        if (strncmp(env, len, leng) == 0 && env[leng] == '=')
+        {
+            val = env + leng + 1;
+            return strdup(val);
+        }
+    }
+    return (NULL);
+}
