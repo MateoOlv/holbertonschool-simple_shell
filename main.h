89170d7c9ce888c4574a9d17da0c27d9d09762c5
MateoOlv <6304@holbertonschool.com>
2023-04-25 14:12:10 +0000

    main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char environ;
+#endif
