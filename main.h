b6999a2400850f7157fd0fa7834325bc8bd40fab
MateoOlv <6304@holbertonschool.com>
2023-04-25 14:15:38 +0000

    main.h

--- main.h
+++ main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <errno.h>
-#include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
