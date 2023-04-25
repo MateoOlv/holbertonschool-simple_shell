ced79399e90643822ea1b6aca4deb1f8f804c12f
coxan33 <coe250@hotmail.com>
2023-04-25 07:15:18 -0700

    add strcat function

--- aux_functions.c
+++ aux_functions.c
@@ -15,4 +15,16 @@ char *_strcpy(char *dest, char *src)
 		continue;
 	return (dest);
 }
+char *_strcat(char *dest, char *src)
+{
+	int len = _strlen(dest);
+	int i;
+
+	for (i = 0; i < _strlen(src); i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
 
