0d6240fb312c6bfe213d6ef1852691737081643f
Mateo1O <compla1@hotmail.com>
2023-04-26 02:11:42 +0000

    commentaries

--- main.h
+++ main.h
@@ -1,15 +1,31 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef main_h
+#define main_h
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <signal.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <stdarg.h>
 
-extern char environ;
+extern char **environ;
+
+/*Principal functions*/
+char *commandfind(char *line);
+int exepath(char **token);
+void tokenize(char **token, char *delim);
+int execom(char *string, char **token);
+void sigintHandler(int sig_num);
+char *_getenv(char *str);
+/*Aux functions*/
+int _strlen(char *str);
+int _strcmp(char *str1, char *str2);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+/*free function*/
+void frees (int n, ...);
+
 #endif
