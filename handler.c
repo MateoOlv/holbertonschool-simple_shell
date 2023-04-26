3015594e4e2e9fd652f76b41c2afb9e307a81ff4
Mateo1O <compla1@hotmail.com>
2023-04-26 02:13:30 +0000

    declared unused variable at start

--- handler.c
+++ handler.c
@@ -2,9 +2,9 @@
 /*La variable num no se usa pero es necesaria para el SIGINT*/
 void sigintHandler(int num)
 {
+    (void)num;
+
 	signal(SIGINT, sigintHandler);
 	printf("\n$ ");
 	fflush(stdout);
-	(void)num
-;
 }
