53d7f1f6cf8ad0a0a8d1da9d3e5dbb54e1397d1c
Mateo1O <compla1@hotmail.com>
2023-04-27 00:02:08 +0000

    betty

--- frees.c
+++ frees.c
@@ -1,15 +1,19 @@
 #include "main.h"
+/**
+ *frees - free function 
+ *@n: amount of variables to free
+ */
 void frees (int n, ...)
 {
- va_list args;
- int i;
- char *c;
+va_list args;
+int i;
+char *c;
 
- va_start(args, n);
- for (i = 0; i < n; i++)
- {
-    c = va_arg(args, char *);
-    free(c);
- }
- va_end(args);
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+c = va_arg(args, char *);
+free(c);
+}
+va_end(args);
 }
