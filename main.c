605d0c59a1fcfba416e67e2b4ecb714f8518affd
Mateo <6304@holbertonstudents.com>
2023-04-29 12:12:32 -0700

    try 2324

--- main.c
+++ main.c
@@ -21,18 +21,21 @@ atty:
 		}
 		if (getline(&input, &buffsize, stdin) == -1)
 		{
+			if (isatty(fileno(stdin)))
+				printf("\n");
 			if (input)
 				free(input);
 			exit(0);
 		}
 		token[0] = strtok(input, delim);
-		if (!token[0])
-			goto atty;
+		if (token[0])
+		{
 		if (strcmp(token[0], "exit") == 0)
 		{
-			free(intput);
-			exit(0);
-		}
+			free(input);
+			exit(0); } }
+		else
+			goto atty;
 		tokenize(token, delim);
 		string = commandfind(token[0]);
 		execom(string, token);
