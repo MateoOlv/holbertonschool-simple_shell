2de7d90fe542a3618cc65cc0939a22999ad56fd5
MateoOlv <124683293+MateoOlv@users.noreply.github.com>
2023-05-03 11:50:13 -0300

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	char delim[] = {" \n\t\r\a"};
 	char *intput = NULL;
-	char **token = {NULL};
+	char *token[80] = {0};
 	char *string = NULL;
 	size_t buffsize = 0;
 
@@ -15,19 +15,24 @@ int main(void)
 	{
 atty:
 		if (isatty(fileno(stdin)))
-		{printf("$ ");
-		fflush(stdout); }
+		{
+			printf("$ ");
+			fflush(stdout);
+		}
 		if (getline(&intput, &buffsize, stdin) == -1)
 		{
 			if (intput)
 				free(intput);
-			exit(0); }
+			exit(0);
+		}
 		token[0] = strtok(intput, delim);
 		if (!token[0])
 			goto atty;
 		if (strcmp(token[0], "exit") == 0)
-		{free(intput);
-		exit(0); }
+		{
+			free(intput);
+			exit(0);
+		}
 		tokenize(token, delim);
 		string = commandfind(token[0]);
 		execom(string, token);
