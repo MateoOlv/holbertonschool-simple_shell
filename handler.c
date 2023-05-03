041aada63d8a2327d348e920af0d58c5591b997d
MateoOlv <124683293+MateoOlv@users.noreply.github.com>
2023-05-03 11:55:01 -0300

    Create handler.c

---
+++ handler.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*La variable num no se usa pero es necesaria para el SIGINT*/
+/**
+*sigintHandler - signal handler
+*@num: number
+*/
+void sigHandler(int num)
+{
+	(void)num;
+
+	signal(SIGINT, sigintHandler);
+	printf("\n$ ");
+	fflush(stdout);
+}
